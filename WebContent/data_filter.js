d47bc660375489f4e5f1a9b80339b96457bccc88
mcr222 <markitus222@hotmail.com>
2016-12-31 12:53:48 +0100

    Filters working (missing adding more filters)

--- WebContent/data_filter.js
+++ WebContent/data_filter.js
@@ -1,4 +1,5 @@
-var filtering_functions_array = [time_filter];
+var filtering_functions_array = [time_filter,discrete_filters];
+//filters return true if data row must be kept
 var number_filtering_functions = filtering_functions_array.length;
 
 function filter_data_row(d) {
@@ -32,9 +33,36 @@ var latestDate = maximumDate;
 
 function time_filter(d) {
 	var date = obtain_Date(d);
-	if( date.getTime()< earliestDate.getTime() || latestDate.getTime()< date.getTime()) {
+	if(date.getTime()< earliestDate.getTime() || latestDate.getTime()< date.getTime()) {
 		return false;
 	}
 	return true;
 	
 }
+
+function equalStrings(str1,str2){
+	if(str1!=null && str2!=null) {
+		return str1.toLowerCase()==str2.toLowerCase();
+	}
+	return false;
+}
+
+function discrete_filters(d) {
+	var data = d.value;
+	if(data==null) {
+		data = d;
+	}
+	var filter;
+	var keep = true;
+	for(var key in filter_table) {
+		filter = filter_table[key];
+		var accepted_values=filter[1];
+		var in_keep_filter = false;
+		for(var i=0;i<accepted_values.length;++i) {
+			in_keep_filter = in_keep_filter || equalStrings(data[filter[0]],accepted_values[i]);
+		}
+		keep = keep && in_keep_filter;
+	}
+	return keep;
+}
+
--- WebContent/filter_panel.js
+++ WebContent/filter_panel.js
@@ -2,7 +2,7 @@ var filter_variable_options = [//"Day description",
                                "Week day",
                                "Day part"];
 var filter_variable_opcions = [//"Descripcio tipus dia",
-                               "Dia setmana",
+                               "Descripcio dia setmana",
                                "Descripcio torn"];
 
 var filter_values_options = [//["Working","Holiday"],
@@ -16,6 +16,8 @@ var filter_values_opcions = [//["Laborable","Festiu"],
 var variable_options = document.getElementById("filter_variable_options");
 var values_options = document.getElementById("filter_values_options");
 var button_add = document.getElementById("add_filter");
+var filters_table_widget = document.getElementById("filters_table");
+
 
 button_add.addEventListener("click",addFilter);
 
@@ -27,6 +29,15 @@ for(var i=0;i<filter_variable_options.length;++i) {
 	option.text = filter_variable_options[i];
 	variable_options.add(option);
 }
+
+var row = filters_table_widget.insertRow();
+row.id = "header";
+var cell0 = row.insertCell(0);
+cell0.innerHTML = "Variable";
+var cell1 = row.insertCell(1);
+cell1.innerHTML = "Keep values";
+var cell2 = row.insertCell(2);
+cell2.innerHTML = "";
 
 var idx=0;
 fillValuesOptions();
@@ -81,7 +92,37 @@ function addFilter() {
 	
 }
 
+var filter_table={};
+
 function addRow(variable, values) {
-	console.log(values);
-	console.log(variable);
+	var length = Object.keys(filter_table).length;
+	filter_table[variable+values] = [variable,values];
+	if(length<Object.keys(filter_table).length) {
+		var row = filters_table_widget.insertRow(-1);
+		row.id = "row"+variable+values;
+		var button_close = document.createElement("button");
+		button_close.id = variable+values;
+		button_close.className = "close_button";
+		button_close.innerHTML = "x";
+		button_close.addEventListener("click",removeFilter);
+		var cell0 = row.insertCell(0);
+		cell0.innerHTML = variable;
+		var cell1 = row.insertCell(1);
+		cell1.innerHTML = values;
+		var cell2 = row.insertCell(2);
+		cell2.appendChild(button_close);
+	}
+	console.log(filter_table);
+	paint_points_in_map();
 }
+
+function removeFilter() {
+	var row = document.getElementById("row"+this.id);
+    row.parentNode.removeChild(row);
+    delete filter_table[this.id];
+	console.log(filter_table);
+	paint_points_in_map();
+}
+
+
+
